f6d00c30a03b7f293a0da038cb66648d61ef95d5
Saurabh <saurab.srivastava@tcs.com>
2020-11-02 15:47:05 +0530

    updated the service URLs

--- src/services/CartService.js
+++ src/services/CartService.js
@@ -1,12 +1,12 @@
 import axios from 'axios';
 
 // IBM Cloud
-/*const CART_BASE_URL = 'http://kart-service-apache.redhat-hackathon-ocp-a39cdf59c11fe9ef74002319618f3999-0000.eu-gb.containers.appdomain.cloud/kart/';
-const ORDER_BASE_URL = 'http://order-service-apache.redhat-hackathon-ocp-a39cdf59c11fe9ef74002319618f3999-0000.eu-gb.containers.appdomain.cloud/order/';*/
+const CART_BASE_URL = 'http://kart-service-apache.redhat-hackathon-ocp-a39cdf59c11fe9ef74002319618f3999-0000.eu-gb.containers.appdomain.cloud/kart/';
+const ORDER_BASE_URL = 'http://order-service-apache.redhat-hackathon-ocp-a39cdf59c11fe9ef74002319618f3999-0000.eu-gb.containers.appdomain.cloud/order/';
 
 // Personal
-const CART_BASE_URL = 'http://kart-service-handykart-product-service.apps.shared-na4.na4.openshift.opentlc.com/kart/';
-const ORDER_BASE_URL = 'http://order-service-handykart-product-service.apps.shared-na4.na4.openshift.opentlc.com/order/';
+/* const CART_BASE_URL = 'http://kart-service-handykart-product-service.apps.shared-na4.na4.openshift.opentlc.com/kart/';
+const ORDER_BASE_URL = 'http://order-service-handykart-product-service.apps.shared-na4.na4.openshift.opentlc.com/order/'; */
 
 class CartService {
     // Create card for a user with list of product items
