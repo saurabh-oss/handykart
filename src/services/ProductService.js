8f29414814381c9def99a248dcef5ee9a07854a2
Saurabh <saurab.srivastava@tcs.com>
2020-11-04 19:14:00 +0530

    updated service URLs for new RedHat environment

--- src/services/ProductService.js
+++ src/services/ProductService.js
@@ -1,12 +1,7 @@
 import axios from 'axios';
 
-// IBM Cloud
-const PRODUCT_API_BASE_URL = 'http://product-service-apache.redhat-hackathon-ocp-a39cdf59c11fe9ef74002319618f3999-0000.eu-gb.containers.appdomain.cloud/product/';
-const REVIEW_API_BASE_URL = 'http://review-service-apache.redhat-hackathon-ocp-a39cdf59c11fe9ef74002319618f3999-0000.eu-gb.containers.appdomain.cloud/review/search/productId/';
-
-// Personal
-/* const PRODUCT_API_BASE_URL = 'http://hackathon-git-handykart-product-service.apps.shared-na4.na4.openshift.opentlc.com/product/';
-const REVIEW_API_BASE_URL = 'http://hackathon-review-git-handykart-product-service.apps.shared-na4.na4.openshift.opentlc.com/review/search/productId/'; */
+const PRODUCT_API_BASE_URL = 'http://product-service-handykart-backend.apps.shared-na4.na4.openshift.opentlc.com/product/';
+const REVIEW_API_BASE_URL = 'http://review-service-handykart-backend.apps.shared-na4.na4.openshift.opentlc.com/review/search/productId/';
 
 class ProductService {
     // List of products based on category name
--- src/services/UserService.js
+++ src/services/UserService.js
@@ -1,10 +1,6 @@
 import axios from 'axios';
 
-// IBM Cloud
-const USER_API_BASE_URL = 'http://user-service-apache.redhat-hackathon-ocp-a39cdf59c11fe9ef74002319618f3999-0000.eu-gb.containers.appdomain.cloud/user/';
-
-// Personal
-//const USER_API_BASE_URL = 'http://hackathon-user-handykart-product-service.apps.shared-na4.na4.openshift.opentlc.com/user/';
+const USER_API_BASE_URL = 'http://user-service-handykart-backend.apps.shared-na4.na4.openshift.opentlc.com/user/';
 
 class UserService {
     registerUser(userObj) {
